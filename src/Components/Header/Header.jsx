57308c54bd225783d5ea577ae29c6f6339e9cb07
Bishoy Adel <bishoy.adel5555@gmail.com>
2024-08-04 04:12:06 +0200

    Strutcture

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,10 @@
+import "./Header.css";
+const Header = () => {
+  return (
+    <div className="Header">
+      <nav className="Header-Container"></nav>
+    </div>
+  );
+};
+
+export default Header;
