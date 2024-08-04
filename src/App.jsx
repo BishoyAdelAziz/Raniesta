be17d9419add4d798e1d0ef3f00d8c75506b24a4
Bishoy Adel <bishoy.adel5555@gmail.com>
2024-08-04 17:45:42 +0200

    Added Home Page Structure outside Routes Tree to be Landing Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import "./App.css";
 import Header from "./Components/Header/Header";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from "./Pages/Home/Home";
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Header />
+        <Home />
         <Routes>
           <Route></Route>
         </Routes>
