cd434b7dd2cabab47b563071181d0d07a2d7fea8
Bishoy Adel <bishoy.adel5555@gmail.com>
2024-08-06 05:16:49 +0200

    Added Assets File to manage Site Requirments

---
+++ src/assets/Assets.js
@@ -0,0 +1,74 @@
+import Gym_1 from "./Gym-1.jpg";
+import Gym_2 from "./Gym-2.jpg";
+import Gym_3 from "./Gym-3.jpg";
+import Gym_4 from "./Gym-4.jpg";
+import Spa_1 from "./spa-1.jpg";
+import Spa_2 from "./spa-2.jpg";
+import Spa_3 from "./spa-3.jpg";
+import Spa_4 from "./spa-4.jpg";
+import Spa_5 from "./spa-5.jpg";
+import Golf_1 from "./golf-1.jpg";
+import Golf_2 from "./golf-2.jpg";
+import Golf_3 from "./golf-3.jpg";
+import Golf_4 from "./golf-4.jpg";
+import Golf_5 from "./golf-5.jpg";
+export const Gym_Photos = [
+  {
+    Photo: Gym_1,
+    Content: "Enjoy your time without waiting",
+  },
+  {
+    Photo: Gym_2,
+    Content: "All the equipment needed to achieve your goals",
+  },
+  {
+    Photo: Gym_3,
+    Content: "Professional assistants to ensure and maximize your benefit",
+  },
+  {
+    Photo: Gym_4,
+    Content:
+      "Empowering Fitness: The Women's Gym Experience at Raniesta Resort",
+  },
+];
+export const Spa_Photos = [
+  { Photo: Spa_1, Content: "Natural Oils Made with Love" },
+  {
+    Photo: Spa_2,
+    Content: "Relaxation And Privacy",
+  },
+  {
+    Photo: Spa_3,
+    Content: "Professionals Spa Attendants",
+  },
+  {
+    Photo: Spa_4,
+    Content: "Get Your meditation",
+  },
+  {
+    Photo: Spa_5,
+    Content: "We care about every detail",
+  },
+];
+export const Golf_Photos = [
+  {
+    Photo: Golf_1,
+    Content: "Spend Your Time with Your Favourites",
+  },
+  {
+    Photo: Golf_2,
+    Content: "Healthy Greens For Better Life",
+  },
+  {
+    Photo: Golf_3,
+    Content: "Golf Cars for better Experience",
+  },
+  {
+    Photo: Golf_4,
+    Content: "Enjoy wherever you are",
+  },
+  {
+    Photo: Golf_5,
+    Content: "Spend Your Time with Your Favourites",
+  },
+];
