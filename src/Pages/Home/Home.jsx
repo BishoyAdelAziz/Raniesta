6dd43728b3d26880aa8e785e8368e0064e6c54bf
Bishoy Adel <bishoy.adel5555@gmail.com>
2024-08-06 05:16:11 +0200

    Home Page Structure

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,6 +7,23 @@ const Home = () => {
           Welcome To <span>Raniesta Resort</span>
         </h3>
       </div>
+      <section className="Section-1">
+        <h4>Services</h4>
+        <div className="Services-Container">
+          <div className="service">
+            <div className="Service-Content">
+              <h5>GYM</h5>
+              <p>
+                Our certified trainers lead a variety of sessions for all
+                levels, from HIIT to yoga. Train on your own schedule with the
+                latest equipment and stunning views. Elevate your stay with
+                fitness at <span className="Resort-name">Raniesta Resort</span>.
+              </p>
+            </div>
+            <div className="Service-Photos"></div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 };
