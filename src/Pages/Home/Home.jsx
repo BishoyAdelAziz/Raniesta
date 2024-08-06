5e03faa36a05ce3dfbc5392dd19b13fcabcee5fc
Bishoy Adel <bishoy.adel5555@gmail.com>
2024-08-06 04:56:52 +0200

    Home Pages Added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,6 +1,14 @@
 import "./Home.css";
 const Home = () => {
-  return <div className="Home"></div>;
+  return (
+    <div className="Home">
+      <div className="Welcome-Message">
+        <h3>
+          Welcome To <span>Raniesta Resort</span>
+        </h3>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
