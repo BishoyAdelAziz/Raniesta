7a1a7fa4499d3f72fb30509403b6bf1aa1097323
Bishoy Adel <bishoy.adel5555@gmail.com>
2024-08-06 04:56:04 +0200

    Updated Header Responsive Logics

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,51 +1,32 @@
 import { Link } from "react-router-dom";
 import { FiAlignRight } from "react-icons/fi";
 import { TfiClose } from "react-icons/tfi";
-
+import { useRef } from "react";
 import "./Header.css";
-import { useState } from "react";
 const Header = () => {
-  const [ShowSide, setShowSide] = useState(false);
+  const navRef = useRef(null);
+  const ShowNavBar = () => {
+    navRef.current.classList.toggle("responsive-nav");
+  };
   return (
-    <div className="Header">
-      <nav className="Header-Container">
-        <div className={ShowSide ? "hide" : "Left-Side"}>
-          <h1>Raniesta Resort</h1>
-        </div>
-        <div className="Center-Side">
-          <Link>Home</Link>
-          <Link>Book Now</Link>
-          <Link>About-us</Link>
-        </div>
-        <div className="Right-Side">
-          <button>Sign-In</button>
-        </div>
-        <div className="Menu-Btn">
-          <FiAlignRight
-            className={ShowSide ? "hide" : "Btn"}
-            onClick={() => setShowSide(!ShowSide)}
-          />
+    <header>
+      <h1>Raniesta Resort</h1>
+      <nav ref={navRef}>
+        <Link to={"/"}>Home</Link>
+        <hr />
+        <Link to={"/about"}>About</Link>
+        <hr />
+        <Link to={"Booking"}>Book Now</Link>
+        <hr />
+        <button className="Log-in">Log-in</button>
+        <div className="Nav-Btn Close-Btn">
+          <TfiClose onClick={ShowNavBar} />
         </div>
       </nav>
-      <div className={ShowSide ? "Column-Nav" : "hide"}>
-        <div className="Close-BTn">
-          <TfiClose onClick={() => setShowSide(!ShowSide)} />
-        </div>
-        <div className="Col-Container">
-          <div className="Col-First">
-            <h1>Raniesta Resort</h1>
-          </div>
-          <div className="Col-Second">
-            <Link onClick={() => setShowSide(!ShowSide)}>Home</Link>
-            <Link onClick={() => setShowSide(!ShowSide)}>Book Now</Link>
-            <Link onClick={() => setShowSide(!ShowSide)}>About-us</Link>
-          </div>
-          <div className="Col-Third">
-            <button>Sign-In</button>
-          </div>
-        </div>
+      <div className="Nav-Btn" onClick={ShowNavBar}>
+        <FiAlignRight />
       </div>
-    </div>
+    </header>
   );
 };
 
