1b3cbf08ab9b79d107aa3fd67e4e15ed60172820
Bishoy Adel <bishoy.adel5555@gmail.com>
2024-08-11 03:25:51 +0200

    added Users , User Object for Login/Signup Function + Validation

--- src/Context/AppContext.jsx
+++ src/Context/AppContext.jsx
@@ -5,11 +5,25 @@ export const AppContext = createContext();
 const ContextProvider = (props) => {
   //Login Dialog Show/Hide Handling State
   const [ShowLogin, setShowLogin] = useState(false);
+  // Users Object (as server)
+  const Users = {};
+  // User Object (as server Request)
+  const User = {
+    _id: 0,
+    firstName: "",
+    lastName: "",
+    email: "",
+    password: "",
+    age: 0,
+    bokkings: {},
+  };
 
   //Context Dispatcher (value prop)
   const Server = {
     ShowLogin,
     setShowLogin,
+    Users,
+    User,
   };
   return (
     //am aware about this .children Error .. i should use prop types in the future while Application Scale Increament
