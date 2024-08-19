368dac3c3ed0911ce8e93d03352ab5c71c759467
Bishoy Adel <bishoy.adel5555@gmail.com>
2024-08-19 03:25:50 +0200

    Booking Page for Routing

--- src/Pages/Booking/Booking.jsx
+++ src/Pages/Booking/Booking.jsx
@@ -1,6 +1,25 @@
 import "./Booking.css";
+import { Rooms } from "../../assets/Assets";
+import Room from "../../Components/Room/Room";
 const Booking = () => {
-  return <div className="Booking">Hello from Book</div>;
+  return (
+    <div className="Booking">
+      <div className="Booking-Entry">
+        <h1>The key to happiness is in your hands</h1>
+        <p>
+          A group of different rooms that meet your desires to ensure an
+          enjoyable time at our resort
+        </p>
+      </div>
+      <div className="Booking-Section">
+        <div className="Rooms">
+          {Rooms.map((item, index) => {
+            return <Room item={item} key={index} />;
+          })}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Booking;
