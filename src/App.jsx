0ed09f4d249d7ddebbaccd71ca6542259b7df40c
Bishoy Adel <bishoy.adel5555@gmail.com>
2024-12-06 15:26:50 +0200

    Lazy Loading for Nav Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,29 @@
 import "./App.css";
+import { lazy, Suspense } from "react";
 import Header from "./Components/Header/Header";
 import { Route, Routes, BrowserRouter } from "react-router-dom";
-import Home from "./Pages/Home/Home";
-import About from "./Pages/About/About";
-import Booking from "./Pages/Booking/Booking";
 import { useContext } from "react";
 import Loginpopup from "./Components/Formpopup/Login/Loginpopup";
 import { AppContext } from "./Context/AppContext";
 function App() {
   const { ShowLogin, setShowLogin } = useContext(AppContext);
+  const Home = lazy(() => import("./Pages/Home/Home"));
+  const About = lazy(() => import("./Pages/About/About"));
+  const Booking = lazy(() => import(`./Pages/Booking/Booking`));
   return (
     <>
       <BrowserRouter>
-        {ShowLogin ? <Loginpopup setShowLogin={setShowLogin} /> : <></>}
-        <div className="App">
-          <Header setShowLogin={setShowLogin} />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/booking" element={<Booking />} />
-          </Routes>
-        </div>
+        <Suspense fallback={<div>Loading..</div>}>
+          {ShowLogin ? <Loginpopup setShowLogin={setShowLogin} /> : <></>}
+          <div className="App">
+            <Header setShowLogin={setShowLogin} />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/booking" element={<Booking />} />
+            </Routes>
+          </div>
+        </Suspense>
       </BrowserRouter>
     </>
   );
